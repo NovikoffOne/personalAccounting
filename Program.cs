c72fad8f806c35ee4c7e9ee908a2df4513d1eae4
NovikoffOne <104835127+NovikoffOne@users.noreply.github.com>
2023-06-16 09:34:14 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace personalAccounting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] fullNameArray = new string[] { "Новиков Иван Олегович", "Назаренко Илья Александрович", "Солодяникна Елаена Егоровна" };
+            string[] jobTitleArray = new string[] { "Фрезировщик", "Менеджер", "Директор" };
+
+            bool isWork = true;
+            int userInput;
+
+            while (isWork)
+            {
+                const int Add = 1;
+                const int DrawDossiers = 2;
+                const int DeletedDossiers = 3;
+                const int SearchNamesakes = 4;
+                const int ExitProgramm = 5;
+
+                DrawMenu(Add, DrawDossiers, DeletedDossiers, SearchNamesakes, ExitProgramm);
+
+                userInput = Convert.ToInt32(ProcessInput());
+
+                switch (userInput)
+                {
+                    case Add:
+                        AddDossierСonclusion(ref fullNameArray, ref jobTitleArray);
+                        break;
+                    case DrawDossiers:
+                        DrawDossier(fullNameArray, jobTitleArray);
+                        break;
+
+                    case DeletedDossiers:
+                        DeleteDossierСonclusion(ref fullNameArray,ref jobTitleArray);
+                        break;
+
+                    case SearchNamesakes:
+                        SearchSecondName(fullNameArray);
+                        break;
+
+                    case ExitProgramm:
+                        Exit(ref isWork);
+                        break;
+
+                    default:
+                        Default();
+                        break;
+                }
+
+                Console.Clear();
+            }
+        }
+
+        static void DrawMenu(int Add, int DrawDossiers, int DeletedDossiers, int SearchNamesakes, int ExitProgramm)
+        {
+            Console.SetCursorPosition(0, 20);
+            Console.WriteLine($"{Add}. Добавить досье\n" +
+                $"{DrawDossiers}. Вывести все досье\n" +
+                $"{DeletedDossiers}. Удалить досье\n" +
+                $"{SearchNamesakes}. Поиск по фамилии\n" +
+                $"{ExitProgramm}. Выход");
+            Console.SetCursorPosition(0, 0);
+        }
+
+        static string ProcessInput()
+        {
+            Console.Write("Выберите пункт : ");
+            return Console.ReadLine();
+        }
+
+        static string GetFullName()
+        {
+            Console.Write("Введите ФИО : ");
+            string fullName = Console.ReadLine();
+            return fullName;
+        }
+
+        static string GetJobtitle()
+        {
+            Console.Write("Введите должность : ");
+            string jobTitle = Console.ReadLine();
+            return jobTitle;
+        }
+
+        static string[] AddDossier(string[] array, string name)
+        {
+            string[] tempArray = new string[array.Length + 1];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                tempArray[i] = array[i];
+            }
+
+            tempArray[array.Length] = name;
+            array = tempArray;
+
+            return array;
+        }
+
+        static void DrawDossier(string[] fullNameArray, string[] jobTitleArray)
+        {
+            for (int i = 0; i < fullNameArray.Length; i++)
+            {
+                Console.Write($"{i + 1}. {fullNameArray[i]} {jobTitleArray[i]}");
+
+                if (i != fullNameArray.Length - 1)
+                {
+                    Console.Write(" - ");
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+        static int GetIndex()
+        {
+            Console.Write("Введите номер досье : ");
+            int index = Convert.ToInt32(Console.ReadLine()) - 1;
+            return index;
+        }
+
+        static string[] DeleteDossier(string[] array, int index)
+        {
+            if (index > array.Length || index < 0)
+            {
+                Console.WriteLine("неверный индекс");
+            }
+
+            string[] tempArray = new string[array.Length - 1];
+
+            for (int i = 0; i < index; i++)
+            {
+                tempArray[i] = array[i];
+            }
+
+            for (int i = index; i < tempArray.Length; i++)
+            {
+                tempArray[i] = array[i + 1];
+            }
+
+            return tempArray;
+        }
+
+        static string GetLastName()
+        {
+            Console.Write("Введите фамилию : ");
+            string lastName = Console.ReadLine();
+            return lastName;
+        }
+
+        static void SearchSecondName(string[] fullNameArray)
+        {
+            string lastName = GetLastName();
+
+            for (int i = 0; i < fullNameArray.Length; i++)
+            {
+                string[] tempArray = fullNameArray[i].Split(' ');
+
+                if (tempArray[0].ToLower() == lastName.ToLower())
+                {
+                    Console.WriteLine(fullNameArray[i]);
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+        static void Exit(ref bool isWork)
+        {
+            Console.WriteLine("До свидания!");
+            Console.ReadKey();
+
+            isWork = false;
+        }
+
+        static void AddDossierСonclusion(ref string[] fullNameArray, ref string[] jobTitleArray)
+        {
+            string fullName = GetFullName();
+            string jobTitle = GetJobtitle();
+
+            fullNameArray = AddDossier(fullNameArray, fullName);
+            jobTitleArray = AddDossier(jobTitleArray, jobTitle);
+        }
+
+        static void DeleteDossierСonclusion(ref string[] fullNameArray, ref string[] jobTitleArray)
+        {
+            int index = GetIndex();
+
+            fullNameArray = DeleteDossier(fullNameArray, index);
+            jobTitleArray = DeleteDossier(jobTitleArray, index);
+        }
+
+        static void Default()
+        {
+            Console.WriteLine("Попробуйте еще раз!");
+            Console.ReadKey();
+        }
+    }
+}
